27f0be60ed495b18678219769315b5ea1b244d9b
Bartosz Cieliński <bart@fullfatthings.com>
2023-07-06 12:41:54 +0100

    upgrade pydantic

--- diff_poetry_lock/settings.py
+++ diff_poetry_lock/settings.py
@@ -1,16 +1,17 @@
 import sys
 from typing import Any
 
-from pydantic import BaseSettings, Field, ValidationError, validator
+from pydantic import field_validator, Field, ValidationError
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    event_name: str = Field(env="github_event_name")  # must be 'pull_request'
-    ref: str = Field(env="github_ref")
-    repository: str = Field(env="github_repository")
-    token: str = Field(env="input_github_token")
-    base_ref: str = Field(env="github_base_ref")
-    lockfile_path: str = Field(env="input_lockfile_path", default="poetry.lock")
+    event_name: str = Field(validation_alias="github_event_name", default="poetry.lock")  # must be 'pull_request'
+    ref: str = Field(validation_alias="github_ref")
+    repository: str = Field(validation_alias="github_repository")
+    token: str = Field(validation_alias="input_github_token")
+    base_ref: str = Field(validation_alias="github_base_ref")
+    lockfile_path: str = Field(validation_alias="input_lockfile_path", default="poetry.lock")
 
     def __init__(self, **values: Any) -> None:  # noqa: ANN401
         try:
@@ -22,7 +23,8 @@ class Settings(BaseSettings):
                 sys.exit(0)
             raise
 
-    @validator("event_name")
+    @field_validator("event_name")
+    @classmethod
     def event_must_be_pull_request(cls, v: str) -> str:  # noqa: N805
         if v != "pull_request":
             raise ValueError("This Github Action can only be run in the context of a pull request")
